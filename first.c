5103d558a252386339e215791f87b962faea100b
zygals <2799024884@qq.com>
2019-10-23 11:33:46 +0800

    firstc

---
+++ first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){	/*this is a function name*/
+	int num;
+	num=1; //this is a assign
+	
+	printf("I am a aimple ");
+	printf("computer.\n");
+	printf("My favorite number is %d because it is first.\n",num);
+
+	return 0;
+
+}
+
+
