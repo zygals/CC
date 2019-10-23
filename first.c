734375dd826e53b65fbcb4ea862281a04c47eb18
zygals <2799024884@qq.com>
2019-10-23 12:40:23 +0800

    add cm

--- first.c
+++ first.c
@@ -4,9 +4,10 @@ int main(void){	/*this is a function name*/
 	int num;
 	num=1; //this is a assign
 	
-	printf("I am a aimple ");
+	printf("I am a aimple "); //this is new 
 	printf("computer.\n");
 	printf("My favorite number is %d because it is first.\n",num);
+	int num2;
 
 	return 0;
 
